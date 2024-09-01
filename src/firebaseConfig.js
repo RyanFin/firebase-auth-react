bae5bb369b0d52ba1b964a931305e94b30e8b4ec
RyanFin <ryanfinlayson125@hotmail.com>
2024-09-01 22:37:14 +0100

    update config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBo_KiK9ylRdyxVZLe__Bcf83ryxzHL72k",
-  authDomain: "fir-auth-project-25be4.firebaseapp.com",
-  projectId: "fir-auth-project-25be4",
-  storageBucket: "fir-auth-project-25be4.appspot.com",
-  messagingSenderId: "33510708473",
-  appId: "1:33510708473:web:d3de6dffb3ba1631b03e28"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
